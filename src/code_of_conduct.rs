898fa78d5086cd7f7936cf9a7772e4237c6ff2f3
Xavier Lange <xrlange@gmail.com>
2016-10-11 14:48:56 -0700

    Landing page plus most of the code of conduct

---
+++ src/code_of_conduct.rs
@@ -0,0 +1,39 @@
+//! San Diego Rust is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any meetup venue, including talks. Meetup participants violating these rules may be expelled from the meetup at the discretion of the organizers.
+//!
+//! Harassment includes, but is not limited to:
+//!
+//!  * Verbal comments that reinforce social structures of domination [related to gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age, religion, [your specific concern here].
+//!  * Sexual images in public spaces
+//!  * Deliberate intimidation, stalking, or following 
+//!  * Harassing photography or recording
+//!  * Sustained disruption of talks or other events
+//!  * Inappropriate physical contact
+//!  * Unwelcome sexual attention
+//!  * Advocating for, or encouraging, any of the above behaviour
+//!
+//! <h2>Enforcement:</h2>
+//!
+//! Participants asked to stop any harassing behavior are expected to comply immediately
+//!
+//! If a participant engages in harassing behaviour, event organisers retain the right to take any actions to keep the event a welcoming environment for all participants. This includes warning the offender or expulsion from the meetup.
+//!
+//! Event organisers may take action to redress anything designed to, or with the clear impact of, disrupting the event or making the environment hostile for any participants.
+//!
+//! We expect participants to follow these rules at all event venues and event-related social activities. We think people should follow these rules outside event activities too!
+//! 
+//! <h2>Reporting</h2>
+//!
+//! If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible. Harassment and other code of conduct violations reduce the value of our event for everyone. We want you to be happy at our event. People like you make our event a better place.
+//!
+//! You can make a report either personally or anonymously.
+//!
+//! <h2>Anonymous Report</h2>
+//!
+//! You can make an anonymous report here [Wufoo form or similar that DOES NOT require an email address to submit. <http://www.wufoo.com/>]
+//!
+//! We can't follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.
+//!
+//!
+//!
+//! This anti-harassment policy is based on [the example policy](http://geekfeminism.wikia.com/wiki/Conference_anti-harassment) from the Geek Feminism wiki, created by the Ada Initiative and other volunteers.
+//!
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+//! <h1>Rust in San Diego, CA!</h1>
+//! 
+//! Come talk with fellow Rustaceans about all things [Rust Lang](https://www.rust-lang.org/)!
+//! Our backgrounds are diverse, from web developers to embedded software and we discuss basic
+//! problem-solving, library authorship, performance, ecosystem changes, and all other things Rust!
+//! 
+//! <h1>Particulars and Location</h1>
+//!
+//! The meetup is held monthly,
+//! **the second Thursday of the month**,
+//! at [the GüdTech offices at 600B St (the Comerica building)](https://www.google.com/maps/place/600+B+St,+San+Diego,+CA+92101).
+//! The meetup starts at 7PM and lasts a few hours. Parking is available in the surrounding streets and it's free after 6PM (be sure to read the posted signs).
+//! 
+//! RSVP for our weekly meetings at [https://www.meetup.com/San-Diego-Rust](https://www.meetup.com/San-Diego-Rust) -- just so we know how much pizza to order!
+//! 
+//! <h1>SDRust Code of Conduct</h1>
+//!
+//! San Diego Rust is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any meetup venue, including talks. Meetup participants violating these rules may be expelled from the meetup at the discretion of the organizers. Our anti-harassment policy can be found at:
+//!
+//! 
+
+pub mod code_of_conduct;
+
+mod Oct_11_2016 {
+	pub fn meetup(){
+	}	
+}
